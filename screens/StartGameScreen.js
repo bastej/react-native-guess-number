bab7d300c801e676f9163d5619ce53db98147916
Sebastian <siatkasebastian@gmail.com>
2019-10-20 19:46:16 +0200

    Added possibility to close keybord when click somewhere outside keyboard (only way to close keyboard on iOS)

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 import Card from "../components/Card";
 import Input from "../components/Input";
@@ -16,30 +23,36 @@ const StartGameScreen = props => {
   };
 
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Let's try to challenge me!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a number</Text>
-        <Input
-          style={styles.input}
-          blurOnSubmit
-          autoCapitalize="none"
-          autoCorrect={false}
-          keyboardType="number-pad"
-          maxLength={2}
-          onChangeText={handleNumberInput}
-          value={enteredValue}
-        />
-        <View style={styles.buttonsContainer}>
-          <View style={styles.button}>
-            <Button color={Colors.primary} title="Confirm" onPress={() => {}} />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={styles.screen}>
+        <Text style={styles.title}>Let's try to challenge me!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize="none"
+            autoCorrect={false}
+            keyboardType="number-pad"
+            maxLength={2}
+            onChangeText={handleNumberInput}
+            value={enteredValue}
+          />
+          <View style={styles.buttonsContainer}>
+            <View style={styles.button}>
+              <Button
+                color={Colors.primary}
+                title="Confirm"
+                onPress={() => {}}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button color={Colors.accent} title="Reset" onPress={() => {}} />
+            </View>
           </View>
-          <View style={styles.button}>
-            <Button color={Colors.accent} title="Reset" onPress={() => {}} />
-          </View>
-        </View>
-      </Card>
-    </View>
+        </Card>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
