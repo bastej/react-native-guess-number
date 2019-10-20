e3b0b78422e04319c2edf14e3dc0efc078ef838a
Sebastian <siatkasebastian@gmail.com>
2019-10-20 18:54:26 +0200

    Created custom Card Component with specific styling - for reusable

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
 import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+import Card from "../components/Card";
+
 const StartGameScreen = props => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Let's try to challenge me!</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a number</Text>
         <TextInput />
         <View style={styles.buttonsContainer}>
           <Button title="Confirm" onPress={() => {}} />
           <Button title="Reset" onPress={() => {}} />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -31,14 +33,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowOffset: { width: 0, height: 2 },
-    shadowColor: "#000",
-    shadowRadius: 6,
-    shadowOpacity: 0.3,
-    elevation: 8,
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 10,
   },
   buttonsContainer: {
     flexDirection: "row",
