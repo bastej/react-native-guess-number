41520603bce0ca0394528131336785cc84859705
Sebastian <siatkasebastian@gmail.com>
2019-10-23 22:06:47 +0200

    Image styling

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,12 +3,18 @@ import { View, Text, StyleSheet, Button, Image } from "react-native";
 
 import PlainText from "../components/PlainText";
 import TitleText from "../components/TitleText";
+import Colors from "../constants/colors";
 
 const GameOverScreen = ({ numberOfRounds, userNumber, onResetGame }) => {
   return (
     <View style={styles.screen}>
-      <TitleText textWeight="bold">Game over!</TitleText>
-      <Image source={require("../assets/game-over.jpg")} />
+      <TitleText textWeight="bold">Sorry...</TitleText>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/game-over.jpg")}
+          style={styles.image}
+        />
+      </View>
       <PlainText>Rounds: {numberOfRounds}</PlainText>
       <PlainText>Number was {userNumber}</PlainText>
       <View style={styles.buttonContainer}>
@@ -26,6 +32,18 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     marginTop: 20,
+  },
+  imageContainer: {
+    width: 240,
+    height: 240,
+    marginVertical: 10,
+    borderColor: Colors.primary,
+    borderRadius: 6,
+    borderWidth: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
